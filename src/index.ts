68884a84b4d98db6205d85ee2fd435c28964bdc3
maruware <me@maruware.com>
2023-03-25 05:20:55 +0900

    server

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,21 @@
-import { add } from "./utils";
+import Fastify, { FastifyInstance, RouteShorthandOptions } from "fastify";
 
-console.log("result", add(1, 2));
+const server: FastifyInstance = Fastify({ logger: true });
+
+server.get("/ping", async (request, reply) => {
+  return { pong: "it worked!" };
+});
+
+const start = async () => {
+  try {
+    await server.listen({ port: 3000 });
+    console.log("listening :3000");
+
+    const address = server.server.address();
+    const port = typeof address === "string" ? address : address?.port;
+  } catch (err) {
+    server.log.error(err);
+    process.exit(1);
+  }
+};
+start();
